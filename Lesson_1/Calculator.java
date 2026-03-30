ec7713db5bcef4e62d5c92d81447a83fded41c34
Вахтанг Шанава <shanava.2004@gmail.com>
2026-03-30 22:54:08 +0300

    Final HW

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,30 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Calculator {
+    public static void main(String[] args) {
+        var firstNum = BigDecimal.valueOf(100000);
+        var secondNum = BigDecimal.valueOf(15);
+        char operation = '%';
+        var result = BigDecimal.valueOf(0);
+
+        if (operation == '+') {
+            result = firstNum.add(secondNum);
+        } else if (operation == '-') {
+            result = firstNum.subtract(secondNum);
+        } else if (operation == '*') {
+            result = firstNum.multiply(secondNum);
+        } else if (operation == '/') {
+            result = firstNum.divide(secondNum, 2, java.math.RoundingMode.HALF_UP);
+        } else if (operation == '%') {
+            result = firstNum.remainder(secondNum);
+        } else if (operation == '^') {
+            result = BigDecimal.valueOf(1);
+            for (int i = 0; i < secondNum.intValue(); i++) {
+                result = result.multiply(firstNum);
+            }
+        }
+
+        System.out.printf("%s %s %s = %s", firstNum, operation, secondNum, result);
+    }
+}
