7ee6b748012e586906425c4356b34cc568720643
Вахтанг Шанава <shanava.2004@gmail.com>
2026-04-06 17:43:24 +0300

    Class HW

---
+++ Lesson_2/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+    String sex = "Мужчина";
+    String firstName = "Александр";
+    float height = 169.1f;
+    float weight = 72.2f;
+    int age = 19;
+
+    void walk() {
+        System.out.println("Прошёл 1 метр");
+    }
+
+    boolean sit() {
+        System.out.println("Сел");
+        return true;
+    }
+
+    void sprint() {
+        System.out.println("Пробежал 10 метров");
+    }
+
+    String speak() {
+        System.out.println("Что-то сказал");
+        return "Что-то";
+    }
+
+    boolean learnJava() {
+        System.out.println("Учит Java");
+        return true;
+    }
+}
