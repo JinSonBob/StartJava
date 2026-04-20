7b46db51f0a2630d7e6f668ce598d7a88806aa05
Вахтанг Шанава <shanava.2004@gmail.com>
2026-04-20 14:24:09 +0300

    Structure change HW3

--- src/com/startjava/lesson_2/method/naming/Methods.java
+++
@@ -1,7 +0,0 @@
-package com.startjava.lesson_2.method.naming;
-
-public class Methods {
-    public static String getCurrentMethodName() {
-        return Thread.currentThread().getStackTrace()[2].getMethodName();
-    }
-}
