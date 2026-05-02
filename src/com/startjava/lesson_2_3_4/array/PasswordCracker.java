84d9d881b78518d581c17d2af54ca263915e2f86
Вахтанг Шанава <shanava.2004@gmail.com>
2026-05-02 11:07:12 +0300

    7-th task from Array HW fix 2

--- src/com/startjava/lesson_2_3_4/array/PasswordCracker.java
+++ src/com/startjava/lesson_2_3_4/array/PasswordCracker.java
@@ -6,14 +6,15 @@ import java.util.Random;
 public class PasswordCracker {
     static void main() throws InterruptedException {
         char[][] passwords = {
-                "\")]H)CCF+N".toCharArray(),
+                "123456".toCharArray(),
                 generatePass(),
                 generatePass(),
                 generatePass()};
 
         for (char[] password : passwords) {
-            boolean[] passwordMetrics = getPasswordMetrics(password);
-            crackPassword(password, passwordMetrics);
+            boolean isPasswordStrong = getPasswordWeaknesses(password);
+            crackPassword(password, isPasswordStrong);
+            System.out.println();
         }
     }
 
@@ -28,23 +29,22 @@ public class PasswordCracker {
         return password;
     }
 
-    private static boolean[] getPasswordMetrics(char[] password) {
+    private static boolean getPasswordWeaknesses(char[] password) {
         char[][] passBlacklist = {
                 "array".toCharArray(),
                 "qwerty".toCharArray(),
                 "123456".toCharArray()};
 
         boolean hasDigit = false;
-        boolean hasLetter = false;
         boolean hasSpecChar = false;
         boolean hasUpperCase = false;
         boolean hasLowerCase = false;
         boolean isBlackListed = false;
+        boolean isStrongPassword = true;
 
         for (char passChar : password) {
             if (Character.isDigit(passChar)) hasDigit = true;
             else if (Character.isLetter(passChar)) {
-                hasLetter = true;
                 if (Character.isUpperCase(passChar)) hasUpperCase = true;
                 if (Character.isLowerCase(passChar)) hasLowerCase = true;
             } else {
@@ -59,49 +59,6 @@ public class PasswordCracker {
             }
         }
 
-        return new boolean[]{hasDigit, hasLetter, hasSpecChar, hasUpperCase, hasLowerCase, isBlackListed};
-    }
-
-    private static void crackPassword(char[] password, boolean[] passwordMetrics)
-            throws InterruptedException {
-        String resetColor = "\u001B[0m";
-        String redColor = "\u001B[31m";
-        String greenColor = "\u001B[32m";
-
-        showSpinner();
-
-        if (!isStrongPassword(password, passwordMetrics)) {
-            System.out.printf("%s✗ Strong password: %s%s%n",
-                    redColor, String.valueOf(password), resetColor);
-            return;
-        }
-        System.out.printf("%s✓ Password cracked: %s%s%n",
-                greenColor, String.valueOf(password), resetColor);
-    }
-
-    private static void showSpinner() throws InterruptedException {
-        char[] spins = {'-', '\\', '|', '/'};
-        int revolutions = spins.length * 3;
-
-        System.out.print("\nCracking password: ");
-
-        for (int i = 0; i <= revolutions; i++) {
-            System.out.print(spins[i % spins.length]);
-            Thread.sleep(250);
-            System.out.print("\b");
-        }
-        System.out.print("\r\r");
-    }
-
-    private static boolean isStrongPassword(char[] password, boolean[] passwordMetrics) {
-        final boolean hasDigit = passwordMetrics[0];
-        final boolean hasLetter = passwordMetrics[1];
-        final boolean hasSpecChar = passwordMetrics[2];
-        final boolean hasUpperCase = passwordMetrics[3];
-        final boolean hasLowerCase = passwordMetrics[4];
-        final boolean isBlackListed = passwordMetrics[5];
-        boolean isStrongPassword = true;
-
         if (isBlackListed) {
             System.out.print("""
                         Не используйте пароли из списка популярных:
@@ -114,23 +71,18 @@ public class PasswordCracker {
                 isStrongPassword = false;
             }
 
-            if (password.length < 8) {
-                System.out.println("Пароль должен быть не менее 8 символов");
+            if (!hasDigit) {
+                System.out.println("Пароль не содержит цифры");
                 isStrongPassword = false;
             }
 
-            if (hasDigit && !hasLetter && !hasSpecChar) {
-                System.out.println("Пароль содержит только цифры");
+            if (!hasUpperCase) {
+                System.out.println("Пароль не содержит буквы верхнего регистра");
                 isStrongPassword = false;
             }
 
-            if (hasLetter && !hasDigit && !hasSpecChar) {
-                System.out.println("Пароль содержит только буквы");
-                isStrongPassword = false;
-            }
-
-            if (hasSpecChar && !hasLetter && !hasDigit) {
-                System.out.println("Пароль содержит только спец. символы");
+            if (!hasLowerCase) {
+                System.out.println("Пароль не содержит буквы нижнего регистра");
                 isStrongPassword = false;
             }
 
@@ -138,12 +90,38 @@ public class PasswordCracker {
                 System.out.println("Пароль не содержит спец. символы");
                 isStrongPassword = false;
             }
-
-            if (!hasLowerCase || !hasUpperCase) {
-                System.out.println("Пароль не содержит буквы нижнего и верхнего регистров");
-                isStrongPassword = false;
-            }
         }
         return isStrongPassword;
+    }
+
+    private static void crackPassword(char[] password, boolean isPasswordStrong)
+            throws InterruptedException {
+        String resetColor = "\u001B[0m";
+        String redColor = "\u001B[31m";
+        String greenColor = "\u001B[32m";
+
+        showSpinner();
+
+        if (isPasswordStrong) {
+            System.out.printf("%s✗ Strong password: %s%s%n",
+                    redColor, String.valueOf(password), resetColor);
+            return;
+        }
+        System.out.printf("%s✓ Password cracked: %s%s%n",
+                greenColor, String.valueOf(password), resetColor);
+    }
+
+    private static void showSpinner() throws InterruptedException {
+        char[] spins = {'-', '\\', '|', '/'};
+        int revolutions = spins.length * 3;
+
+        System.out.print("Cracking password: ");
+
+        for (int i = 0; i <= revolutions; i++) {
+            System.out.print(spins[i % spins.length]);
+            Thread.sleep(250);
+            System.out.print("\b");
+        }
+        System.out.print("\r\r");
     }
 }
