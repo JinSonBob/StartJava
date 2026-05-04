8b97810a7ea8e97f6646dce78abafed066f6c900
Вахтанг Шанава <shanava.2004@gmail.com>
2026-05-04 16:23:12 +0300

    7-th task from Array HW fix 4

--- src/com/startjava/lesson_2_3_4/array/PasswordCracker.java
+++ src/com/startjava/lesson_2_3_4/array/PasswordCracker.java
@@ -4,56 +4,58 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class PasswordCracker {
+    private static final char[][] PASSWORD_BLACKLIST = {
+            "array".toCharArray(),
+            "qwerty".toCharArray(),
+            "123456".toCharArray()};
+
     static void main() throws InterruptedException {
         char[][] passwords = {
                 "123456".toCharArray(),
-                generatePass(),
-                generatePass(),
-                generatePass()};
+                generatePassword(),
+                generatePassword(),
+                generatePassword()};
 
         for (char[] password : passwords) {
-            boolean isPasswordStrong = getPasswordWeaknesses(password);
-            crackPassword(password, isPasswordStrong);
+            boolean isStrong = isStrongPassword(password);
+            crackPassword(password, isStrong);
             System.out.println();
         }
     }
 
-    private static char[] generatePass() {
+    private static char[] generatePassword() {
         Random random = new Random();
-        int passLength = random.nextInt(6, 13);
-        char[] password = new char[passLength];
+        int passwordLength = random.nextInt(6, 13);
+        char[] password = new char[passwordLength];
 
-        for (int i = 0; i < passLength; i++) {
-            password[i] = (char) random.nextInt(33, 126);
+        for (int i = 0; i < passwordLength; i++) {
+            password[i] = (char) random.nextInt(33, 127);
         }
         return password;
     }
 
-    private static boolean getPasswordWeaknesses(char[] password) {
-        char[][] passBlacklist = {
-                "array".toCharArray(),
-                "qwerty".toCharArray(),
-                "123456".toCharArray()};
-
+    private static boolean isStrongPassword(char[] password) {
         boolean hasDigit = false;
         boolean hasSpecChar = false;
         boolean hasUpperCase = false;
         boolean hasLowerCase = false;
         boolean isBlackListed = false;
-        boolean isStrongPassword = true;
+        boolean isStrong = true;
 
-        for (char passChar : password) {
-            if (Character.isDigit(passChar)) hasDigit = true;
-            else if (Character.isLetter(passChar)) {
-                if (Character.isUpperCase(passChar)) hasUpperCase = true;
-                if (Character.isLowerCase(passChar)) hasLowerCase = true;
+        for (char passwordChar : password) {
+            if (Character.isDigit(passwordChar)) {
+                hasDigit = true;
+            } else if (Character.isUpperCase(passwordChar)) {
+                hasUpperCase = true;
+            } else if (Character.isLowerCase(passwordChar)) {
+                hasLowerCase = true;
             } else {
                 hasSpecChar = true;
             }
         }
 
-        for (char[] blacklistedPass : passBlacklist) {
-            if (Arrays.equals(blacklistedPass, password)) {
+        for (char[] blacklistedPassword : PASSWORD_BLACKLIST) {
+            if (Arrays.equals(blacklistedPassword, password)) {
                 isBlackListed = true;
                 break;
             }
@@ -64,41 +66,41 @@ public class PasswordCracker {
                     Не используйте пароли из списка популярных:
                     https://nordpass.com/most-common-passwords-list
                     """);
-            isStrongPassword = false;
+            isStrong = false;
         }
         if (password.length == 0) {
             System.out.println("Пароль не может быть пустым");
-            isStrongPassword = false;
+            isStrong = false;
         }
         if (password.length < 8) {
             System.out.println("Пароль содержит менее 8-и символов");
-            isStrongPassword = false;
+            isStrong = false;
         }
 
         if (!hasDigit) {
             System.out.println("Пароль не содержит цифры");
-            isStrongPassword = false;
+            isStrong = false;
         }
 
         if (!hasUpperCase) {
             System.out.println("Пароль не содержит буквы верхнего регистра");
-            isStrongPassword = false;
+            isStrong = false;
         }
 
         if (!hasLowerCase) {
             System.out.println("Пароль не содержит буквы нижнего регистра");
-            isStrongPassword = false;
+            isStrong = false;
         }
 
         if (!hasSpecChar) {
             System.out.println("Пароль не содержит спец. символы");
-            isStrongPassword = false;
+            isStrong = false;
         }
-        return isStrongPassword;
+        return isStrong;
     }
 
     @SuppressWarnings("checkstyle:AvoidEscapedUnicodeCharacters")
-    private static void crackPassword(char[] password, boolean isPasswordStrong)
+    private static void crackPassword(char[] password, boolean isStrong)
             throws InterruptedException {
         String resetColor = "\u001B[0m";
         String redColor = "\u001B[31m";
@@ -106,7 +108,7 @@ public class PasswordCracker {
 
         showSpinner();
 
-        if (isPasswordStrong) {
+        if (isStrong) {
             System.out.printf("%s✗ Strong password: %s%s%n",
                     redColor, String.valueOf(password), resetColor);
             return;
@@ -121,7 +123,7 @@ public class PasswordCracker {
 
         System.out.print("Cracking password: ");
 
-        for (int i = 0; i <= revolutions; i++) {
+        for (int i = 0; i < revolutions; i++) {
             System.out.print(spins[i % spins.length]);
             Thread.sleep(250);
             System.out.print("\b");
