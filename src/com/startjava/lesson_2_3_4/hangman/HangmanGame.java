610f25c4bf598c3f7cdbdf0f13e740bade51add7
Вахтанг Шанава <shanava.2004@gmail.com>
2026-05-16 12:31:05 +0300

    Hangman from final HW fix 4

--- src/com/startjava/lesson_2_3_4/hangman/HangmanGame.java
+++ src/com/startjava/lesson_2_3_4/hangman/HangmanGame.java
@@ -27,7 +27,7 @@ public class HangmanGame {
 
     public HangmanGame() {
         Random random = new Random();
-        secretWord = secretWords[random.nextInt(secretWords.length)];
+        secretWord = secretWords[random.nextInt(secretWords.length)].toUpperCase();
         wordMask = new StringBuilder("_".repeat(secretWord.length()));
         wrongLetters = new StringBuilder();
         errorsCount = 0;
@@ -45,13 +45,12 @@ public class HangmanGame {
         if (isWin()) {
             System.out.println("Вы угадали слово\n");
         } else {
-            System.out.printf("Вы не угадали слово: %s%n",
-                    String.valueOf(secretWord).toUpperCase());
+            System.out.printf("Вы не угадали слово: %s%n", secretWord);
         }
     }
 
     private boolean isWin() {
-        return wordMask.indexOf("_") == -1;
+        return secretWord.contentEquals(wordMask);
     }
 
     private boolean hasAttempt() {
@@ -74,13 +73,11 @@ public class HangmanGame {
     }
 
     private void printGuessedLetters() {
-        System.out.printf("Отгадано: %s", wordMask.toString().toUpperCase());
+        System.out.printf("Отгадано: %s", wordMask);
     }
 
     private void printWrongLetters() {
-        for (int i = 0; i < wrongLetters.length(); i++) {
-            System.out.print(Character.toUpperCase(wrongLetters.charAt(i)));
-        }
+        System.out.print(wrongLetters);
     }
 
     private void takeGuess(Scanner scanner) {
@@ -89,31 +86,19 @@ public class HangmanGame {
 
         do {
             System.out.print("Введите букву: ");
-            input = scanner.nextLine().toLowerCase();
+            input = scanner.nextLine().toUpperCase();
         } while (!isValidInput(input) || isAlreadyUsed(input));
 
         letter = input.charAt(0);
 
         if (hasLetter(letter)) {
-            System.out.printf("Отгадана буква %s%n", Character.toUpperCase(letter));
+            System.out.printf("Отгадана буква %s%n", letter);
             updateWordMask(letter);
             if (errorsCount > 0) errorsCount--;
         } else {
-            System.out.printf("В слове нет буквы %s%n", Character.toUpperCase(letter));
+            System.out.printf("В слове нет буквы %s%n", letter);
             wrongLetters.append(letter);
             errorsCount++;
-        }
-    }
-
-    private boolean hasLetter(char letter) {
-        return secretWord.indexOf(letter) != -1;
-    }
-
-    private void updateWordMask(char letter) {
-        for (int i = 0; i < secretWord.length(); i++) {
-            if (secretWord.charAt(i) == letter) {
-                wordMask.setCharAt(i, letter);
-            }
         }
     }
 
@@ -129,13 +114,23 @@ public class HangmanGame {
         return true;
     }
 
-    private boolean isAlreadyUsed(String input) {
-        char letter = input.charAt(0);
-        if (wrongLetters.toString().indexOf(letter) != -1 ||
-                wordMask.toString().indexOf(letter) != -1) {
-            System.out.printf("Буква %s уже вводилась%n", Character.toUpperCase(letter));
+    private boolean isAlreadyUsed(String letter) {
+        if (wrongLetters.indexOf(letter) != -1 || wordMask.indexOf(letter) != -1) {
+            System.out.printf("Буква %s уже вводилась%n", letter);
             return true;
         }
         return false;
+    }
+
+    private boolean hasLetter(char letter) {
+        return secretWord.indexOf(letter) != -1;
+    }
+
+    private void updateWordMask(char letter) {
+        for (int i = 0; i < secretWord.length(); i++) {
+            if (secretWord.charAt(i) == letter) {
+                wordMask.setCharAt(i, letter);
+            }
+        }
     }
 }
