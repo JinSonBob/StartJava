59ac19f8b7dbee4264113d6ee8703b12c48c10df
Вахтанг Шанава <shanava.2004@gmail.com>
2026-05-25 23:28:18 +0300

    Menu from BookShelf* HW fix 1

--- src/com/startjava/lesson_2_3_4/bookshelf/Menu.java
+++ src/com/startjava/lesson_2_3_4/bookshelf/Menu.java
@@ -1,22 +1,16 @@
 package com.startjava.lesson_2_3_4.bookshelf;
 
 public enum Menu {
-    ADD(1, "Добавить книгу"),
-    FIND(2, "Найти книгу по названию"),
-    REMOVE(3, "Удалить книгу по названию"),
-    CLEAR(4, "Очистить шкаф"),
-    EXIT(5, "Закончить работу");
+    ADD("Добавить книгу"),
+    FIND("Найти книгу по названию"),
+    REMOVE("Удалить книгу по названию"),
+    CLEAR("Очистить шкаф"),
+    EXIT("Закончить работу");
 
-    private final int optionNum;
     private final String option;
 
-    Menu(int optionNum, String option) {
-        this.optionNum = optionNum;
+    Menu(String option) {
         this.option = option;
-    }
-
-    public int getOptionNum() {
-        return optionNum;
     }
 
     public String getOption() {
